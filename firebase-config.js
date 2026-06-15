8e67731a45d626a3f09b7d84cead3f24321299d7
Max-294 <cutewanda@gmail.com>
2026-06-15 19:51:42 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  enabled: true,
+  apiKey: "AIzaSyAA6nN-Fp0SexOTuGCBJA_KAa-2ouj_avQ",
+  authDomain: "landcraft-731e1.firebaseapp.com",
+  projectId: "landcraft-731e1",
+  storageBucket: "landcraft-731e1.firebasestorage.app",
+  messagingSenderId: "485496704890",
+  appId: "1:485496704890:web:53200d9d7514aa8a82d0c3",
+  measurementId: "G-5BXHM4V6NX"
+};
